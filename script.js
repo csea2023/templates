368971b0acc64b69eaf80800369cf3850b7b6ab5
Kaustubh S <kasben10@gmail.com>
2023-06-20 22:27:01 +0530

    2nd commit

--- script.js
+++ script.js
@@ -36,60 +36,6 @@ function validateForm() {
     }
   }
 }
-/*function convertToMp3() {
-  var youtubeLink = document.getElementById("yt").value;
-
-  // Check if the input is a valid YouTube link
-  if (!youtubeLink.includes("youtube.com/watch?v=")) {
-      alert("Invalid YouTube link");
-      return;
-  }
-
-  // Extract the video ID from the YouTube link
-  var videoId = youtubeLink.split("v=")[1];
-
-  // Create an HTTP POST request to the server
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://your-server-url.com/convert", true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-
-  // Set up the data to be sent to the server
-  var data = JSON.stringify({
-      videoId: videoId
-  });
-
-  // Handle the response from the server
-  xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-          var response = JSON.parse(xhr.responseText);
-          if (response.success) {
-              alert("MP3 file conversion successful");
-              // Perform any additional actions upon successful conversion
-          } else {
-              alert("MP3 file conversion failed");
-              // Perform any error handling actions
-          }
-      }
-  };
-
-  // Send the request with the data to the server
-  xhr.send(data);
-}
-var chords = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
-
-        // Get the container element
-        var chordContainer = document.getElementById('chordContainer');
-
-        // Loop through the chords and create chord elements
-        for (var i = 0; i < chords.length; i++) {
-            // Create a new chord element
-            var chordElement = document.createElement('div');
-            chordElement.className = 'chord';
-            chordElement.textContent = chords[i];
-
-            // Append the chord element to the container
-            chordContainer.appendChild(chordElement);
-        }*/
 
 
 function uploadFile() {
